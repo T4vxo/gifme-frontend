9aa9edce7befb9cb926beae8694868a32a5b5200
schully <dagr99002@utb.vaxjo.se>
2018-12-17 11:45:20 +0100

    ajax url call refactor

--- src/public/random_gif/main.js
+++ src/public/random_gif/main.js
@@ -1,37 +1,56 @@
-$(document).ready(() => {
+
+$(document).ready(init)
+
+/**
+ *  called when document has been loaded
+ */
+function init() {
     console.log('document is ready');
-    
-    $("#btn").on("click", () => {
 
-        var query = $(".input").val();
-        console.log(query);
-
-        $.ajax({
-            type: "GET",
-            url: `http://localhost:8080/api/randgif/gif/${encodeURIComponent(query)}`,
-            dataType: "json",
-            success: (data) => {
-                console.log(data);
-                $('.gifImg').attr('src', data.url);
-                
-            },
-            statusCode: {
-                404: function () {
-                    alert("page not found");
-                },
-                500: function () {
-                    alert("server error");
-                },
-            }
-        });
-    });
+    $("#btn").on("click", searchButton());
 
     setupListeners()
-})
 
+}
+
+/**
+ * performs GET request from backend
+ */
+function search(query = $(".input").val()) {
+
+
+    $.ajax({
+        type: "GET",
+        url: getBackendUrl(`/randgif/gif/${encodeURIComponent(query)}`),
+        dataType: "json",
+        success: onSearchSuccess,
+        statusCode: {
+            404: function () {
+                alert("page not found");
+            },
+            500: function () {
+                alert("server error");
+            },
+        }
+    });
+}
+
+/**
+ * Fetches gif on a successfull search
+ * @param {*} data 
+ */
+function onSearchSuccess(data) {
+    console.log(data);
+    $('.gifImg').attr('src', data.url);
+}
+
+/**
+ * Keyboard listener
+ * Calls 'triggerSearch' by pressing 'Enter/Return'
+ */
 function setupListeners() {
     $('#input').on('keydown', e => {
-        if (e.keyCode == 13){
+        if (e.keyCode == 13) {
             //  Enter pressed
             triggerSearch()
             e.preventDefault();
@@ -43,5 +62,5 @@ function setupListeners() {
  * Triggers a GIF search.
  */
 function triggerSearch() {
-    $('#btn').click();
+    searchButton();
 }
